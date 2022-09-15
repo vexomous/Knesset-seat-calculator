c95ff2c58bc5c8174ec55297e99747bf660f2322
vexomous <108953661+vexomous@users.noreply.github.com>
2022-09-16 01:27:54 +0300

    Update calculator.py
    Rewrote to use google form response sheet

--- calculator.py
+++ calculator.py
@@ -1,25 +1,42 @@
 import csv
+import json
 import operator
 from re import L
 from objects import Party, SharingAgreement
 import math
+import requests
+import re
+from collections import Counter
+import matplotlib.pyplot as plt
+import numpy as np
+import secrets
 
 THRESHOLD_PERCENT = 3.25
 TOTAL_SEATS = 120
-VOTE_FILE = 'votes.csv'
+VOTESHARE_DOC = 'votesharing.json'
 
 def load_votes():
-    with open(VOTE_FILE, 'r', encoding="utf8") as f:
-        a = [{k: v for k, v in row.items()} for row in csv.DictReader(f, skipinitialspace=True)]
-        return a
+    response = requests.get(
+        f'https://docs.google.com/spreadsheet/ccc?key={secrets.SHEET_ID}&output=csv')
+    assert response.status_code == 200, 'Wrong status code'
+    responses = response.content.decode('utf-8').split('\r\n')[1:]
+    responses = [x[20:] for x in responses]
+    counts = Counter(responses)
+    return counts
+
 
 def create_parties(parties_dict):
     parties = list()
-    for party in parties_dict:
-        parties.append(Party(party["name"], party["votes"], party["votesharing"]))
+    with open(VOTESHARE_DOC, 'r') as f:
+        votesharings = json.load(f)
+    for party, votes in parties_dict.items():
+        if party != '':
+            parties.append(Party(party, votes, votesharings[party]))
     for party in parties:
-        party.set_partner(next((x for x in parties if x.name == party.votesharing), None))
+        party.set_partner(
+            next((x for x in parties if x.name == party.votesharing), None))
     return parties
+
 
 def calc_seat_leftover(party, receiving, seat_parameter):
     receiving_new_total = receiving.leftovers + party.leftovers
@@ -28,6 +45,7 @@ def calc_seat_leftover(party, receiving, seat_parameter):
         receiving.set_leftovers(receiving_new_total - seat_parameter)
         party.set_leftovers(0)
 
+
 def bader_ofer(legit_parties, seat_parameter):
     for party in legit_parties:
         seats = math.floor(party.votes/seat_parameter)
@@ -35,7 +53,6 @@ def bader_ofer(legit_parties, seat_parameter):
         party.set_seats(seats)
         party.set_leftovers(leftovers)
     left_to_distribute = 120 - sum(party.seats for party in legit_parties)
-    distrib_lists = dict()
     left_to_partnerize = legit_parties
     partnerships = list()
     for party in left_to_partnerize:
@@ -43,29 +60,38 @@ def bader_ofer(legit_parties, seat_parameter):
         if party.partner != None:
             left_to_partnerize.remove(party.partner)
     while left_to_distribute > 0:
-        partnerships = sorted(partnerships, key=operator.attrgetter("standard"), reverse=True)
-        print("adding to", partnerships[0].party1.name)
+        partnerships = sorted(
+            partnerships, key=operator.attrgetter("standard"), reverse=True)
         partnerships[0].add_seat()
         left_to_distribute -= 1
     for partnership in partnerships:
         partnership.distrib_bonus_seats()
 
+
 def calculate_seats(parties):
     total_votes = sum(party.votes for party in parties)
+    for party in parties:
+        party.set_percentage(round(party.votes/total_votes*100, 2))
     threshold_votes = total_votes * THRESHOLD_PERCENT / 100
-    legit_parties = [party for party in parties if party.votes >= threshold_votes]
+    legit_parties = [
+        party for party in parties if party.votes >= threshold_votes]
     legit_votes = sum(party.votes for party in legit_parties)
     seat_parameter = legit_votes / TOTAL_SEATS
     bader_ofer(legit_parties, seat_parameter)
-    
 
 
 def main():
     parties_dict = load_votes()
     parties = create_parties(parties_dict)
     calculate_seats(parties)
+    parties = sorted(parties, key=lambda x: (
+        x.seats, x.percentage), reverse=True)
     for party in parties:
-        print(party.name, party.seats)
+        if party.seats > 0:
+            print(party.name, party.seats)
+        else:
+            print(f"{party.name} {party.percentage}%")
+
 
 if __name__ == "__main__":
     main()
